ec5210ef68aab02e39e2edbc42ddac510a263c41
Juan Morales <juan.morales@quby.com>
2017-06-15 23:55:47 +0200

    some settings

--- lambda.web/settings.py
+++ lambda.web/settings.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# This secret is used for CSRF protection.  Not yet implemented.
 secret = "klMp9a5EHN_a21AsGS8vj9Xg5fca"
 
 # This allows you to run flask in dev mode
 DEBUG = True
+
+# This allows you to run in "lambda" mode, or in local mode
+ONLINE=False
