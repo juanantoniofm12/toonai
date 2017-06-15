8dbac21296fe0c804e6462b33356fd02d37a40f6
Juan Morales <juan.morales@quby.com>
2017-06-15 23:54:04 +0200

    alternate logging facilities

--- lambda.web/app.py
+++ lambda.web/app.py
@@ -1,19 +1,25 @@
 
 # -*- coding: utf-8 -*-
 
+import sys
 from toonapp import app
 
 if __name__ == '__main__':
     if not app.debug:
         import logging
-        from logging.handlers import TimedRotatingFileHandler
+        from logging.handlers import TimedRotatingFileHandler, StreamHandler
         file_handler = TimedRotatingFileHandler(
-            "myapp.log",
+            "lambdastuff.log",
             when="D",
             backupCount=10
             )
         file_handler.setLevel(logging.WARNING)
-        app.logger.addHandler(file_handler)
+        #TODO: Maybe for a lambda is better to just log  stuff to stdout
+        if app.config["ONLINE"]:
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            app.logger.addHandler(stdout_handler)
+        else:
+            app.logger.addHandler(file_handler)
 
     app.debug = app.config['DEBUG']
     app.secret_key = 'super secret key'
